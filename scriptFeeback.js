c3c51bdc7bc8c66862adb81e31f2bd611301897f
FedeYozora <federicobonfi@gmail.com>
2023-10-20 12:09:32 +0200

    feedback page finished and on page 2 more difficulties are selectable

--- scriptFeeback.js
+++ scriptFeeback.js
@@ -1,13 +1,44 @@
 // Get the stars elements
-const stars = document.querySelectorAll(".stars i");
+const stars = Array.from(document.querySelectorAll(".stars i"));
+const feedback = document.querySelector(".textBox");
 
 // Add a click event listener to each star
 stars.forEach((star, index1) => {
   star.addEventListener("click", () => {
-    stars.forEach((star, index2) => {
-      index1 >= index2
-        ? star.classList.add("active")
-        : star.classList.remove("active");
-    });
+    // Remove the active class from all stars
+    stars.forEach(star => star.classList.remove("active"));
+
+    // Add the active class to the clicked star and all previous stars
+    stars.slice(0, index1 + 1).forEach(star => star.classList.add("active"));
   });
+});
+
+document.getElementById("submitForm").addEventListener("click", e => {
+  e.preventDefault();
+
+  const selectedStar = document.getElementsByClassName("active").length;
+
+  // Display a different feedback message based on the selected star value
+  if (
+    selectedStar === 1 ||
+    selectedStar === 2 ||
+    selectedStar === 3 ||
+    selectedStar === 4 ||
+    selectedStar === 5
+  ) {
+    feedback.innerHTML = `<h1>We're sorry to hear that you had a bad experience.</h1>
+        <h3>How can we improve?</h3>`;
+    document.querySelector(".rating-box").innerHTML = "";
+    document.querySelector("main").innerHTML = "";
+  } else if (selectedStar === 6 || selectedStar === 7) {
+    feedback.innerHTML = `<h1>Thank you for your feedback.</h1>
+        <h3>We're glad you had a good experience.</h3>`;
+    document.querySelector(".rating-box").innerHTML = "";
+    document.querySelector("main").innerHTML = "";
+  } else if (selectedStar === 8 || selectedStar === 9 || selectedStar === 10) {
+    feedback.innerHTML = `<h1>Thank you for your positive feedback!</h1>
+        <h3>We're happy to hear that you had a great experience.</h3>`;
+    document.querySelector(".rating-box").innerHTML = "";
+    document.querySelector("main").innerHTML = "";
+  }
 });
--- scriptQuiz.js
+++ scriptQuiz.js
@@ -1,98 +1,298 @@
-const questions = [
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "What does CPU stand for?",
-    correct_answer: "Central Processing Unit",
-    incorrect_answers: [
-      "Central Process Unit",
-      "Computer Personal Unit",
-      "Central Processor Unit",
-    ],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
-    correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "The logo for Snapchat is a Bell.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question:
-      "Pointers were not used in the original C programming language; they were added later on in C++.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "What is the most preferred image format used for logos in the Wikimedia database?",
-    correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "In web design, what does CSS stand for?",
-    correct_answer: "Cascading Style Sheet",
-    incorrect_answers: [
-      "Counter Strike: Source",
-      "Corrective Style Sheet",
-      "Computer Style Sheet",
-    ],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "What is the code name for the mobile operating system Android 7.0?",
-    correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question: "On Twitter, what is the character limit for a Tweet?",
-    correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Linux was first created as an alternative to Windows XP.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "Which programming language shares its name with an island in Indonesia?",
-    correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"],
-  },
-];
+const questions = [];
+function setDifficulty(difficulty) {
+  questions.length = 0; // Clear the array
+
+  if (difficulty === "easy") {
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "What does CPU stand for?",
+      correct_answer: "Central Processing Unit",
+      incorrect_answers: [
+        "Central Process Unit",
+        "Computer Personal Unit",
+        "Central Processor Unit",
+      ],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+      correct_answer: "Final",
+      incorrect_answers: ["Static", "Private", "Public"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "The logo for Snapchat is a Bell.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question:
+        "Pointers were not used in the original C programming language; they were added later on in C++.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the most preferred image format used for logos in the Wikimedia database?",
+      correct_answer: ".svg",
+      incorrect_answers: [".png", ".jpeg", ".gif"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "In web design, what does CSS stand for?",
+      correct_answer: "Cascading Style Sheet",
+      incorrect_answers: [
+        "Counter Strike: Source",
+        "Corrective Style Sheet",
+        "Computer Style Sheet",
+      ],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the code name for the mobile operating system Android 7.0?",
+      correct_answer: "Nougat",
+      incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "On Twitter, what is the character limit for a Tweet?",
+      correct_answer: "140",
+      incorrect_answers: ["120", "160", "100"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "Linux was first created as an alternative to Windows XP.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "Which programming language shares its name with an island in Indonesia?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "C", "Jakarta"],
+    });
+  } else if (difficulty === "medium") {
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "All of the following programs are classified as raster graphics editors EXCEPT:",
+      correct_answer: "Inkscape",
+      incorrect_answers: ["Paint.NET", "GIMP", "Adobe Photoshop"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "Which one of these is not an official development name for a Ubuntu release?",
+      correct_answer: "Mystic Mansion",
+      incorrect_answers: ["Trusty Tahr", "Utopic Unicorn", "Wily Werewolf"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "The open source program Redis is a relational database server.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "Which coding language was the #1 programming language in terms of usage on GitHub in 2015?",
+      correct_answer: "JavaScript",
+      incorrect_answers: ["C#", "Python", "PHP"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "In HTML, which non-standard tag used to be be used to make elements scroll across the viewport?",
+      correct_answer: "&lt;marquee&gt;&lt;/marquee&gt;",
+      incorrect_answers: [
+        "&lt;scroll&gt;&lt;/scroll&gt;",
+        "&lt;move&gt;&lt;/move&gt;",
+        "&lt;slide&gt;&lt;/slide&gt;",
+      ],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question: ".at is the top-level domain for what country?",
+      correct_answer: "Austria",
+      incorrect_answers: ["Argentina", "Australia", "Angola"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "When did the online streaming service &quot;Mixer&quot; launch?",
+      correct_answer: "2016",
+      incorrect_answers: ["2013", "2009", "2011"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "Which programming language was developed by Sun Microsystems in 1995?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "Solaris OS", "C++"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "Approximately how many Apple I personal computers were created?",
+      correct_answer: "200",
+      incorrect_answers: ["100", "500", "1000"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "In programming, what do you call functions with the same name but different implementations?",
+      correct_answer: "Overloading",
+      incorrect_answers: ["Overriding", "Abstracting", "Inheriting"],
+    });
+  } else if (difficulty === "hard") {
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question:
+        "What was the name of the security vulnerability found in Bash in 2014?",
+      correct_answer: "Shellshock",
+      incorrect_answers: ["Heartbleed", "Bashbug", "Stagefright"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question: "How many Hz does the video standard PAL support?",
+      correct_answer: "50",
+      incorrect_answers: ["59", "60", "25"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question:
+        "What is the name of the process that sends one qubit of information using two bits of classical information?",
+      correct_answer: "Quantum Teleportation",
+      incorrect_answers: [
+        "Super Dense Coding",
+        "Quantum Entanglement",
+        "Quantum Programming",
+      ],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question:
+        "America Online (AOL) started out as which of these online service providers?",
+      correct_answer: "Quantum Link",
+      incorrect_answers: ["CompuServe", "Prodigy", "GEnie"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question: "What internet protocol was documented in RFC 1459?",
+      correct_answer: "IRC",
+      incorrect_answers: ["HTTP", "HTTPS", "FTP"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question:
+        "Which of these is not a key value of Agile software development?",
+      correct_answer: "Comprehensive documentation",
+      incorrect_answers: [
+        "Individuals and interactions",
+        "Customer collaboration",
+        "Responding to change",
+      ],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question: "Who invented the &quot;Spanning Tree Protocol&quot;?",
+      correct_answer: "Radia Perlman",
+      incorrect_answers: ["Paul Vixie", "Vint Cerf", "Michael Roberts"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question: "The acronym &quot;RIP&quot; stands for which of these?",
+      correct_answer: "Routing Information Protocol",
+      incorrect_answers: [
+        "Runtime Instance Processes",
+        "Regular Interval Processes",
+        "Routine Inspection Protocol",
+      ],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question: "What was the name of the first Bulgarian personal computer?",
+      correct_answer: "IMKO-1",
+      incorrect_answers: ["Pravetz 82", "Pravetz 8D", "IZOT 1030"],
+    });
+    questions.push({
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "hard",
+      question:
+        "What is the codename of the eighth generation Intel Core micro-architecture launched in October 2017?",
+      correct_answer: "Coffee Lake",
+      incorrect_answers: ["Sandy Bridge", "Skylake", "Broadwell"],
+    });
+  }
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach(button => (button.style.display = "none"));
+  startQuiz();
+}
 
 const questionSpace = document.getElementById("questionArea");
 const answerButton = document.getElementById("buttonRow");
@@ -129,6 +329,7 @@ function resetTimer() {
 function startQuiz() {
   currentQuestionIndex = 0;
   score = 0;
+  document.querySelector("#donut-segment").classList.add("animation");
   showQuestion();
 }
 
@@ -196,5 +397,3 @@ let handleIncorrectAns = function () {
   this.style.borderColor = "red";
   this.removeEventListener("click", handleIncorrectAns);
 };
-
-startQuiz();
